0018e1596f1c740c8d2f41f0b9c8116ed950a6ed
abahernest <abahernesto@gmail.com>
2023-12-01 07:15:34 +0100

    chore: setup api versioning
    configure base url to include 'api/v1' by setting default API version to '1' and global prefix to 'api'

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'v',
+  });
+
+  app.setGlobalPrefix('api');
 
   app.use(helmet());
   app.enableCors();
